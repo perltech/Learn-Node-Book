e32d630b8180fef91608c8421d82a839dfd86998
perltech <noah.perlmutter@gmail.com>
2018-06-11 17:31:07 -0600

    add if file exists program

---
+++ chap3/identity.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+class FileObject {
+    constructor() {
+        this.filename = '';
+    }
+
+    file_exists(cb) {
+        console.log(`About to open: ${this.filename}`);
+
+        fs.open(this.filename, 'r', (err, handle) => {
+            if (err) {
+                console.log(`Can't open ${this.filename}`);
+                cb(err);
+                return;
+            }
+            fs.close(handle, () => {});
+            cb(null, true);
+        });
+    }
+}
+
+const fo = new FileObject();
+fo.filename = "file_that_does_not_exist";
+fo.file_exists((err, results) => {
+    if (err) {
+        console.log(`\nError opening file: ${JSON.stringify(err)}`);
+        return;
+    }
+    console.log("file exists!");
+});
