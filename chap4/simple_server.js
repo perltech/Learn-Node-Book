193746f6f0340b27ba349e11d57860e3f6231ca8
perltech <noah.perlmutter@gmail.com>
2018-06-12 18:21:28 -0600

    creating headers for responses

--- chap4/simple_server.js
+++ chap4/simple_server.js
@@ -1,10 +1,31 @@
+const fs = require("fs");
 const http = require("http");
 const PORT = 8080;
 
+const loadAlbumList = cb => {
+    //Assuming every directory in "albums" is, in fact, an album
+    fs.readdir("albums", (err, files) => {
+        if (err) {
+            cb(err);
+            return;
+        }
+        cb(null, files);
+    });
+};
+
 const handleIncomingRequest = (req, res) => {
     console.log(`INCOMING REQUEST: ${req.method} ${req.url}`);
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ error: null }) + "\n");
+    loadAlbumList((err, albums) => {
+        if (err) {
+            res.writeHead(503, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(err + "\n"));
+            return;
+        }
+        const out = { error: null, data: { albums: albums } };
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(out) + "\n");
+    });
+
 };
 
 const server = http.createServer(handleIncomingRequest);
